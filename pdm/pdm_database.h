a2418b597eb29d5c1769b63803f44347c3e3f0ad
mike yang <18604713262@163.com>
2022-11-25 22:27:44 -0500

    Sqlite integration

---
+++ pdm/pdm_database.h
@@ -0,0 +1,16 @@
+//
+// Created by Mike Yang on 11/25/2022.
+//
+
+#ifndef PDM_PLATFORMS_UI_PDM_DATABASE_H
+#define PDM_PLATFORMS_UI_PDM_DATABASE_H
+namespace PDM
+{
+class pdm_database {
+  pdm_database();
+};
+
+}
+
+
+#endif //PDM_PLATFORMS_UI_PDM_DATABASE_H
