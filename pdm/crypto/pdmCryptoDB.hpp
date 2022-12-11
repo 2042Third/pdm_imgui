ede89fd2a991a995c614984f39539dca160f3bab
mike yang <18604713262@163.com>
2022-12-10 23:09:10 -0500

    Custom encryption database complete.

--- pdm/crypto/pdmCryptoDB.hpp
+++ pdm/crypto/pdmCryptoDB.hpp
@@ -47,7 +47,8 @@ public:
     uint8_t key_pair[key_size+1];
     cc20_utility::gen_key_nonce_pair(key_pair,key_size); // generate the key and nonce randomly
 
-    std::string str_key((char*)key_pair);
+    std::string str_key; str_key.resize(key_size);
+    memcpy(str_key.data(),((char*)key_pair),key_size);
     String testKey(str_key);
 
     std::cout<<"\nKey"<<std::endl;
@@ -64,23 +65,24 @@ public:
     size_t key_size= cc20_utility::nonce_key_pair_size();
 
     // Initialize normal buffer
-    std::vector<uint8_t> key_buffer, wrapped_buffer, wrapping_buffer;
+    std::vector<uint8_t> key_buffer, wrapped_buffer;
+    std::string wrapping_buffer;
     key_buffer.reserve(key_size+1); // source
     wrapping_buffer.reserve(wrappingKey.size()+1);
     wrapped_buffer.reserve(key_size+NONCE_SIZE+POLY_SIZE+1); // destination
 
     // Copy from key to normal buffer
     copy_to_normal_buffer(key_buffer.data(),key,key_size);
-    copy_to_normal_buffer(wrapping_buffer.data(),wrappingKey,wrappingKey.size());
+    copy_to_normal_buffer((uint8_t*)wrapping_buffer.data(),wrappingKey,wrappingKey.size());
 
     // Encrypt normally
-    PDM_BRIDGE_MOBILE::cmd_enc((const uint8_t*)key_buffer.data()
+    cmd_enc(( uint8_t*)key_buffer.data()
                                 ,key_size
                                 ,(uint8_t*)wrapped_buffer.data()
-                                ,(const uint8_t*)wrapping_buffer.data());
+                                ,wrapping_buffer);
 
     // Copy everything back out
-    wrappedKey.write(wrapped_buffer.data(),wrapped_buffer.size());
+    wrappedKey.write(wrapped_buffer.data(),key_size+NONCE_SIZE+POLY_SIZE);
 
     // TODO: cleanup the stack buffers.
 
@@ -99,30 +101,49 @@ public:
 
   void unwrapKey(Buffer &key, const Buffer &wrappedKey, const Buffer &wrappingKey) const override {
     std::cout<<"PDM database key unwrap"<<std::endl;
+    std::cout<<" unwrap key size: "<< wrappedKey.size()<<std::endl;
     size_t key_size= cc20_utility::nonce_key_pair_size();
 
     // Initialize normal buffer
-    std::vector<uint8_t> key_buffer, wrapped_buffer, wrapping_buffer;
+    std::vector<uint8_t> key_buffer, wrapped_buffer;
+    std::string wrapping_buffer;
     key_buffer.reserve(key_size+1); // destination
     wrapping_buffer.reserve(wrappingKey.size()+1);
-    wrapped_buffer.reserve(wrappedKey.size()); // source
+    wrapped_buffer.reserve(key_size+NONCE_SIZE+POLY_SIZE+1); // source
 
-    // Decrypt like a normal thing
-    PDM_BRIDGE_MOBILE::cmd_dec((const uint8_t*)wrapped_buffer.data(),key_size
-        ,(uint8_t*)key_buffer.data()
-        ,(const uint8_t*)wrapping_buffer.data());
+    // Copy into the normal buffer
+//    key.append(wrappedKey);
+    copy_to_normal_buffer(wrapped_buffer.data(),wrappedKey,key_size+NONCE_SIZE+POLY_SIZE);
+    copy_to_normal_buffer((uint8_t*)wrapping_buffer.data(),wrappingKey,wrappingKey.size());
 
-    // Copy out everything
-    key.write(key_buffer.data(),wrappedKey.size()- (NONCE_SIZE+POLY_SIZE));
-    // TODO: cleanup the stack buffers.
-
-    std::cout<<"\nKey"<<std::endl;
+    std::cout<<"\npre Key"<<std::endl;
     print_stats((uint8_t*)key_buffer.data()
         ,key_size
         ,0);
 
     std::cout<<"wrappedKey"<<std::endl;
-    print_stats((uint8_t*)wrapping_buffer.data(),key_size+NONCE_SIZE+POLY_SIZE);
+    print_stats((uint8_t*)wrapped_buffer.data(),key_size+NONCE_SIZE+POLY_SIZE);
+
+    std::cout<<"wrappingKey"<<std::endl;
+    print_stats((uint8_t*)wrapping_buffer.data(),wrappingKey.size(),0);
+
+    // Decrypt like a normal thing
+    cmd_dec(( uint8_t*)wrapped_buffer.data()
+                                ,key_size+NONCE_SIZE+POLY_SIZE
+                                ,(uint8_t*)key_buffer.data()
+                                ,wrapping_buffer);
+
+    // Copy out everything
+    key.write(key_buffer.data(),wrappedKey.size()- (NONCE_SIZE+POLY_SIZE));
+    // TODO: cleanup the stack buffers.
+
+    std::cout<<"\npost Key"<<std::endl;
+    print_stats((uint8_t*)key_buffer.data()
+        ,key_size
+        ,0);
+
+    std::cout<<"wrappedKey"<<std::endl;
+    print_stats((uint8_t*)wrapped_buffer.data(),key_size+NONCE_SIZE+POLY_SIZE);
 
     std::cout<<"wrappingKey"<<std::endl;
     print_stats((uint8_t*)wrapping_buffer.data(),wrappingKey.size(),0);
@@ -140,7 +161,7 @@ private:
   }
 
   static void print_stats(const uint8_t* a,size_t size,int binary=1) {
-    std::string ac ((const char*)a,0,size);
+    std::string ac ; ac.resize(size); memcpy(ac.data(),a,size);
     std::cout<< "Size : \""<<ac.size()<<"\""<<std::endl;
     if(!binary)std::cout<< "Plain: "<<ac<<std::endl;
     std::cout<< " Hax : \""<<stoh(ac)<<"\""<<std::endl;
