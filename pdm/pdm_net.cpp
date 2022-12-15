eda8ea937740fe1aacefe44fcf507a3829d8471f
mike yang <18604713262@163.com>
2022-12-15 14:24:38 -0500

    Removed pdm_network deps on curl, and change to asio

--- pdm/pdm_net.cpp
+++ pdm/pdm_net.cpp
@@ -10,17 +10,9 @@ namespace PDM {
 
   int network::signin_action(const std::string&a) {
 
-//    net.post(a, actions.signinURL);
     return 1;
   }
 
-//  template<typename T>
-//  std::string network::get_json (std::map<T,T>&a){
-//    using json = nlohmann::json;
-//    json j(a);
-//    std::string signin_data =to_string(j);
-//    return std::move(signin_data);
-//  }
 
   network::network() {
   }
