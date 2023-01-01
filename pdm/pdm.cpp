e7f13a18c55711b2db077f1e5d847cf872074eac
mike yang <18604713262@163.com>
2022-12-31 19:08:30 -0500

    Network calls, made local static function pointer overwrite.

--- pdm/pdm.cpp
+++ pdm/pdm.cpp
@@ -23,31 +23,34 @@ namespace PDM {
 
   int Runtime::signin_action(const std::string&a, NetWriter* wt_in, const char* password){
     std::string ps = password;
-    struct c {
+    struct NetCallBack_ {
       static size_t _callback(char *data, size_t size, size_t nmemb, void *userp) {
-
+        auto *wt = (struct NetObj *)userp;
+        if (wt->userinfo.status == "success") { // The sign in is successful
+          MD5 md5; md5.add(wt->userinfo.email.c_str(),wt->userinfo.email.size());
+          std::string file_names = md5.getHash(); // md5 encode user email
+          // Check or create the appropriate db
+          auto*rt = (PDM::Runtime*)wt->pdm_runtime;
+          if (!rt->get_user_loc(file_names)){
+            const std::filesystem::path confp(rt->conf_loc), datap(rt->data_loc);
+            std::filesystem::create_directories(confp.parent_path());
+            std::filesystem::create_directories(datap.parent_path());
+            // TODO: What to do when user first use this computer to login
+          }
+          std::cout<< "conf_loc: "<< rt->conf_loc<<std::endl;
+          std::cout<< "data_loc: "<< rt->data_loc<<std::endl;
+          rt->user_conf->open_db(rt->conf_loc.c_str(),"pdmnotes",8);
+          rt->user_data->open_db(rt->data_loc.c_str(),wt->data.c_str(),wt->data.size());
+        } else {
+          // TODO: Deal with the failed login
+        }
         return post_callback_signin(data,  size,  nmemb, userp); /* we copied this many bytes */
       }
     };
+    wt_in->pdm_runtime = this;
+    wt_in->data = ps;
+    signin_post(a,wt_in,NetCallBack_::_callback); // Call network post for Sign In action
 
-    signin_post(a,wt_in,c::_callback); // Call network post for Sign In action
-    if (wt.userinfo.status == "success") { // The sign in is successful
-      MD5 md5; md5.add(wt.userinfo.email.c_str(),wt.userinfo.email.size());
-      std::string file_names = md5.getHash(); // md5 encode user email
-      // Check or create the appropriate db
-      if (!get_user_loc(file_names)){
-        const std::filesystem::path confp(conf_loc), datap(data_loc);
-        std::filesystem::create_directories(confp.parent_path());
-        std::filesystem::create_directories(datap.parent_path());
-        // TODO: What to do when user first use this computer to login
-      }
-      std::cout<< "conf_loc: "<< conf_loc<<std::endl;
-      std::cout<< "data_loc: "<< data_loc<<std::endl;
-      user_conf->open_db(conf_loc.c_str(),"pdmnotes",8);
-      user_data->open_db(data_loc.c_str(),ps.c_str(),ps.size());
-    } else {
-      // TODO: Deal with the failed login
-    }
     return 1;
   }
 
