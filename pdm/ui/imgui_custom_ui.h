de772997d49feba10f588290ccff9f726710a48a
mike yang <18604713262@163.com>
2023-01-25 15:00:37 -0500

    ios

--- pdm/ui/imgui_custom_ui.h
+++ pdm/ui/imgui_custom_ui.h
@@ -8,9 +8,7 @@
 #include "imgui.h"
 
 namespace ImGui {
-
   bool SingleLineSelectableText(const char * label, const char* buffer, size_t buffer_size, int original_color=0);
-
 } // ImGui
 
 #endif //PDM_PLATFORMS_UI_IMGUI_CUSTOM_UI_H
